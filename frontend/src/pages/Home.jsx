de179afeade0c09fb8b6b805ebd782549131251d
Persistent-A <apurva.mili@gmail.com>
2023-06-22 18:03:17 -0400

    changes in home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div style={{textAlign:"center", fontSize:"large", minHeight:"70vh", color:"white"}} className="images" >
-      <div style={{paddingTop:"40px"}}>Medical center</div>
+      <div style={{paddingTop:"30px"}}>Medical center</div>
     </div>
   )
 }
